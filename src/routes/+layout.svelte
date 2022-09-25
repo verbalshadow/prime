bdd37f916929b35cc0422c793723b73f34241101
Joshua L. Blocher <verbalshadow@gmail.com>
2022-09-24 22:57:13 -0600

    more mocking out layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,13 @@
 <script>
+    import Header from '$lib/components/header.svelte'
+    import Footer from '$lib/components/footer.svelte'
+    import '@picocss/pico/'
     /** @type {import('./$types').LayoutData} */
     export let data;
 </script>
+
+<Header />
+<main>
+  <slot />
+</main>
+<Footer />
