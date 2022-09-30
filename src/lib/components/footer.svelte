9c7a42c48b2e22c230d3292cb9f5fdcf2e6296e9
Joshua L. Blocher <verbalshadow@gmail.com>
2022-09-30 09:48:57 -0600

    update footer

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -3,8 +3,10 @@
 </script>
 
 <footer class="container">
-    <small>Copyright verbalshadow 2022 • 
-        Built with <a href="https://hive.io" target="_blank">Hive</a>, 
+    <small>
+        Copyright verbalshadow 2022 • Built with 
+        <a href="https://hive.io" target="_blank">Hive</a>, 
         <a href="https://kit.svelte.dev" target="_blank">SvelteKit</a> & 
-        <a href="https://picocss.com" target="_blank">PicoCSS</a></small>
+        <a href="https://picocss.com" target="_blank">PicoCSS</a>
+    </small>
 </footer>
