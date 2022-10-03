73746e9877eba79b4bde12cbbeeeaf25c8557dc0
Joshua L. Blocher <verbalshadow@gmail.com>
2022-10-02 18:57:32 -0600

    Make HiveStore.js work and Create Settings.js

--- src/lib/stores/HiveStore.js
+++ src/lib/stores/HiveStore.js
@@ -1,16 +1,26 @@
 import { writable } from 'svelte/store'
+import { hiveId, hiveAPIs, hideCrossPosts } from './Settings';
 import { Client } from "@hiveio/dhive";
 
 export const posts = writable([]);
+// export const tags = writable(new Set());
 
-const client = new Client(["https://api.hive.blog", "https://api.hivekings.com", "https://anyx.io", "https://api.openhive.network"]);
+const client = new Client(hiveAPIs);
 
 var query = {
-  tag: 'verbalshadow', // This tag is used to filter the results by a specific post tag
+  tag: hiveId, // This tag is used to filter the results by a specific post tag
   limit: 25, // This limit allows us to limit the overall results returned to 5
 };
 
 client.database.getDiscussions('blog', query)
 .then((results) =>{
-  posts.update(value => [...value, ...results]);
+  posts.update(value => [...value, ...results
+    .reduce((hold, current) =>{
+      let post = current;
+      post.json_metadata = JSON.parse(current.json_metadata);
+      if (post.json_metadata.tags.includes("cross-post") && hideCrossPosts) return hold;
+      hold.push(post);
+      return hold;
+    }, [])
+  ]);
 })
---
+++ src/lib/stores/Settings.js
@@ -0,0 +1,7 @@
+// Site Configuration
+export const hiveId = "verbalshadow";
+export const brand = "small world"
+export const hideCrossPosts = true;
+
+// Configure Hive fallbacks if one or more fail
+export const hiveAPIs = ["https://api.hive.blog", "https://api.hivekings.com", "https://anyx.io", "https://api.openhive.network"]
