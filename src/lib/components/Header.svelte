b4adc03ad4bed1a3a1178330391d533ebade0714
Joshua L. Blocher <verbalshadow@gmail.com>
2022-10-03 18:49:11 -0600

    fix relative locations in nav

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -7,15 +7,15 @@
     <nav class="container-fluid">
       <ul>
         <li>
-            <a href="./" class="contrast"><strong>{brand}</strong></a>
+            <a href="/" class="contrast"><strong>{brand}</strong></a>
         </li>
       </ul>
       <ul>
         <li>
-            <a href="./blog">Blog</a>
+            <a href="/blog">Blog</a>
         </li>
         <li>
-            <a href="./about">About</a>
+            <a href="/about">About</a>
         </li>
       </ul>
     </nav>
