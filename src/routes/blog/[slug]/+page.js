7b68323826d859dd0a85b73c89c07a19412022fc
Joshua L. Blocher <verbalshadow@gmail.com>
2022-10-04 22:15:45 -0600

    Got slug pages working

--- src/routes/blog/[slug]/+page.js
+++ src/routes/blog/[slug]/+page.js
@@ -1,10 +1,16 @@
 /** @type {import('./$types').PageLoad} */
 import { posts } from '../../../lib/stores/HiveStore'
+// import { compile } from 'mdsvex';
+
 let workingPosts;
 posts.subscribe( value => {
-    workingPosts = value;
+  workingPosts = value;
 })
 
 export async function load({ params }){
-    return workingPosts.find(element => element.permlink === params.slug)
+  // let goodPost = workingPosts.find(element => element.permlink === params.slug);
+  // const body = await compile(goodPost.body);
+  // const title = goodPost.title;
+  return await workingPosts.find(element => element.permlink === params.slug);
+  // return {name: title, content: body?.code}
 }
--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,11 +1,41 @@
 <script>
-    /** @type {import('./$types').PageData} */
-    export let data;
+  /** @type {import('./$types').PageData} */
+  import { marked } from 'marked';
+  export let data;
+  
+  let body = marked.parse(data.body)
 </script>
 
 <article id="article">
-    <h2>{data.title}</h2>
-    <p>Nullam dui arcu, malesuada et sodales eu, efficitur vitae dolor. Sed ultricies dolor non ante vulputate hendrerit. Vivamus sit amet suscipit sapien. Nulla iaculis eros a elit pharetra egestas. Nunc placerat facilisis cursus. Sed vestibulum metus eget dolor pharetra rutrum.</p>
-    <slot />
-    <footer><small>Duis nec elit placerat, suscipit nibh quis, finibus neque.</small></footer>
+  <header>
+    <hgroup>
+      <h1>{data.title}</h1>
+        {#if data.json_metadata.description != undefined}
+      <h2>{data.json_metadata.description}</h2>
+        {:else}
+      <h2> </h2>
+        {/if}
+    </hgroup> 
+    <ul>
+      {#each data.json_metadata.tags as tag }
+        <li>{tag}</li>
+      {/each}
+    </ul> 
+  </header>
+  {@html body}
+  <footer><small>Duis nec elit placerat, suscipit nibh quis, finibus neque.</small></footer>
  </article>
+
+ <style>
+    ul {
+      display: inline;
+      margin-left: -18px;
+    }
+    ul li{
+      display: inline-flex;
+      border-radius: 4px;
+      background-color: var(--secondary);
+      margin: 4px;
+      padding: 3px 6px 3px 6px ;
+    }
+  </style>
