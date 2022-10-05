4f70f337c6d42660b320b66cbe1f4b952ce13512
Joshua L. Blocher <verbalshadow@gmail.com>
2022-10-04 22:10:35 -0600

    update Header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -3,20 +3,20 @@
     import { brand } from '../stores/Settings.js';
 </script>
 
-<div class="hero" data-theme="dark">
-    <nav class="container-fluid">
-      <ul>
-        <li>
-            <a href="/" class="contrast"><strong>{brand}</strong></a>
-        </li>
-      </ul>
-      <ul>
-        <li>
-            <a href="/blog">Blog</a>
-        </li>
-        <li>
-            <a href="/about">About</a>
-        </li>
-      </ul>
-    </nav>
-  </div><!-- ./ Hero -->
+<div >
+  <nav class="container-fluid">
+    <ul>
+      <li>
+        <a href="/" class="secondary"><strong>{brand}</strong></a>
+      </li>
+    </ul>
+    <ul>
+      <li>
+        <a href="/blog">Blog</a>
+      </li>
+      <li>
+        <a href="/about">About</a>
+      </li>
+    </ul>
+  </nav>
+</div>
