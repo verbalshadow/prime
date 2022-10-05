feab659be4ecf9f09a799ed212f3d573d3ab3baa
Joshua L. Blocher <verbalshadow@gmail.com>
2022-10-04 23:55:00 -0600

    add hideReblogs option Settings unimplemented

--- src/lib/stores/Settings.js
+++ src/lib/stores/Settings.js
@@ -2,6 +2,7 @@
 export const hiveId = "verbalshadow";
 export const brand = "small world"
 export const hideCrossPosts = true;
+export const hideReblogs = false; // TODO: implement
 
 // Configure Hive fallbacks if one or more fail
 export const hiveAPIs = ["https://api.hive.blog", "https://api.hivekings.com", "https://anyx.io", "https://api.openhive.network"]
