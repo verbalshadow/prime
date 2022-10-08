d16a985c9cbf714cb68aef63bd100d193a8c6b93
Joshua L. Blocher <verbalshadow@gmail.com>
2022-10-08 10:53:07 -0600

    tidy file

--- src/routes/blog/[slug]/+page.js
+++ src/routes/blog/[slug]/+page.js
@@ -1,11 +1,11 @@
 /** @type {import('./$types').PageLoad} */
 import { posts } from '../../../lib/stores/HiveStore'
+export const prerender = false;
 
 let workingPosts;
 posts.subscribe( value => {
   workingPosts = value;
 })
-export const prerender = false;
 export async function load({ params }){
   return await workingPosts.find(element => element.permlink === params.slug);
 }
