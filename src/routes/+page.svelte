3d927b1b9de5e13cb76e24ea3064dcbca7c7c4ae
Joshua L. Blocher <verbalshadow@gmail.com>
2022-10-20 19:32:24 -0600

    add shoutouts to main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,15 @@
 <script>
     /** @type {import('./$types').PageData} */
+	import ShoutOut from '../lib/components/ShoutOut.svelte';
 </script>
+
 <hgroup>
 <h1>Welcome to <strong>Prime</strong></h1> 
 <h2>The Quasi-Static Blog for Hive.</h2>
 </hgroup>
+
+<div class="grid">
+    <ShoutOut name="V4V.app" description="Make payments bridge the Lightning & Hive Networks." alttag="V4V app logo" image="https://v4v.app/hivelightning-dark.png" url="https://v4v.app/"/>
+    <ShoutOut />
+    <ShoutOut />
+</div>
