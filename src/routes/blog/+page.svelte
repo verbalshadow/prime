729cba10ffe9dd409c1a5ddb7e292e128b9a472e
Joshua L. Blocher <verbalshadow@gmail.com>
2022-12-07 14:26:56 -0700

    add await to blog page

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,15 +1,19 @@
 <script>
-    /** @type {import('./$types').PageData} */
-    import {posts} from '$lib/stores/HiveStore.js';
-    import Teaser from '$lib/components/Teaser.svelte';
+	/** @type {import('./$types').PageData} */
+	import { posts } from '$lib/stores/HiveStore.js';
+	import Teaser from '$lib/components/Teaser.svelte';
+	import Loading from '$lib/components/Loading.svelte';
 </script>
 
 <svelte:head>
-  <title>Blog Posts</title>
+	<title>Blog Posts</title>
 </svelte:head>
 
 <h1>Recent Posts</h1>
-{#each $posts as art}
-    <Teaser art={art}/>
-{/each}
-
+{#await $posts}
+	<Loading />
+{:then $posts}
+	{#each $posts as art}
+		<Teaser {art} />
+	{/each}
+{/await}
